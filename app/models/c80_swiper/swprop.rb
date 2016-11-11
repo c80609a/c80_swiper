80633de0dc29af1e886f9c9852de687de1f6c4ed
C80609A <c080609a@gmail.com>
2016-11-11 07:22:12 +0300

    master: models: habtm заготовка

--- app/models/c80_swiper/swprop.rb
+++ app/models/c80_swiper/swprop.rb
@@ -1,4 +1,5 @@
 module C80Swiper
   class Swprop < ActiveRecord::Base
+    # has_and_belongs_to_many :swsliders
   end
 end
--- app/models/c80_swiper/swslider.rb
+++ app/models/c80_swiper/swslider.rb
@@ -1,4 +1,5 @@
 module C80Swiper
   class Swslider < ActiveRecord::Base
+    # has_and_belongs_to_many :swprops
   end
 end
