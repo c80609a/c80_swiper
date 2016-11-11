1edff63fdf32632d2f2a898c1a12ee2e12a1f5a8
C80609A <c080609a@gmail.com>
2016-11-11 07:49:47 +0300

    master: minor: utf8 fix.

--- db/migrate/20161111034340_create_c80_swiper_swprops.rb
+++ db/migrate/20161111034340_create_c80_swiper_swprops.rb
@@ -1,6 +1,6 @@
 class CreateC80SwiperSwprops < ActiveRecord::Migration
   def change
-    create_table :c80_swiper_swprops do |t|
+    create_table :c80_swiper_swprops, :options => 'COLLATE=utf8_unicode_ci' do |t|
 
       t.timestamps null: false
     end
--- db/migrate/20161111041555_create_c80_swiper_swsliders.rb
+++ db/migrate/20161111041555_create_c80_swiper_swsliders.rb
@@ -1,6 +1,6 @@
 class CreateC80SwiperSwsliders < ActiveRecord::Migration
   def change
-    create_table :c80_swiper_swsliders do |t|
+    create_table :c80_swiper_swsliders, :options => 'COLLATE=utf8_unicode_ci' do |t|
       t.string :title
 
       t.timestamps null: false
