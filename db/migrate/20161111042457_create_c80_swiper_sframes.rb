4c269367f0315fb8cbbe0f0d3fc730f6775d7d3f
C80609A <c080609a@gmail.com>
2016-11-11 13:56:02 +0300

    master: minor: migrate: default true for 'enabled'.

--- db/migrate/20161111042457_create_c80_swiper_sframes.rb
+++ db/migrate/20161111042457_create_c80_swiper_sframes.rb
@@ -4,7 +4,7 @@ class CreateC80SwiperSframes < ActiveRecord::Migration
       t.string :title
       t.string :image
       t.integer :ord
-      t.boolean :enabled
+      t.boolean :enabled, default: true
       t.references :swslider, index: true
 
       t.timestamps null: false
