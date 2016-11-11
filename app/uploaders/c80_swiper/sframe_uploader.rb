42f8839d3b3df8ce4a852c2d8409469ea57890ee
C80609A <c080609a@gmail.com>
2016-11-11 11:19:06 +0300

    master: uploader: SframeUploader: заменил Proc.new на begin..rescue при генерации thumbs.

--- app/uploaders/c80_swiper/sframe_uploader.rb
+++ app/uploaders/c80_swiper/sframe_uploader.rb
@@ -6,38 +6,43 @@ module C80Swiper
     process :resize_to_limit => [1024,768]
 
     version :thumb_big_one do
-      Proc.new {
+      begin
         p = C80Swiper::Swprop.first
         process :resize_to_fill => [p.big_one_width, p.big_one_height]
-      }
+      rescue => e
+      end
     end
 
     version :thumb_preview do
-      Proc.new {
+      begin
         p = C80Swiper::Swprop.first
         process :resize_to_fill => [p.preview_width, p.preview_height]
-      }
+      rescue => e
+      end
     end
 
     version :thumb_sm do
-      Proc.new {
+      begin
         p = C80Swiper::Swprop.first
         process :resize_to_fill => [p.thumb_sm_width, p.thumb_sm_height]
-      }
+      rescue => e
+      end
     end
 
     version :thumb_md do
-      Proc.new {
+      begin
         p = C80Swiper::Swprop.first
         process :resize_to_fill => [p.thumb_md_width, p.thumb_md_height]
-      }
+      rescue => e
+      end
     end
 
     version :thumb_lg do
-      Proc.new {
+      begin
         p = C80Swiper::Swprop.first
         process :resize_to_fill => [p.thumb_lg_width, p.thumb_lg_height]
-      }
+      rescue => e
+      end
     end
 
     def store_dir
